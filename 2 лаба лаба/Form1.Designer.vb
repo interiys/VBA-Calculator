ede4a520b39ec317b9ef2c7bf0c9960d4d82e7ac
Your Name <you@example.com>
2023-03-10 20:10:16 +0300

    test

---
+++ 2 лаба лаба/Form1.Designer.vb
@@ -0,0 +1,352 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBoxIs = New System.Windows.Forms.TextBox()
+        Me.bt7 = New System.Windows.Forms.Button()
+        Me.bt4 = New System.Windows.Forms.Button()
+        Me.bt1 = New System.Windows.Forms.Button()
+        Me.bt0 = New System.Windows.Forms.Button()
+        Me.bt8 = New System.Windows.Forms.Button()
+        Me.bt5 = New System.Windows.Forms.Button()
+        Me.bt2 = New System.Windows.Forms.Button()
+        Me.btpm = New System.Windows.Forms.Button()
+        Me.bt9 = New System.Windows.Forms.Button()
+        Me.bt6 = New System.Windows.Forms.Button()
+        Me.bt3 = New System.Windows.Forms.Button()
+        Me.btpoint = New System.Windows.Forms.Button()
+        Me.btdel = New System.Windows.Forms.Button()
+        Me.btpr = New System.Windows.Forms.Button()
+        Me.btmin = New System.Windows.Forms.Button()
+        Me.btplus = New System.Windows.Forms.Button()
+        Me.btButton4 = New System.Windows.Forms.Button()
+        Me.btce = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btis = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.sqrt = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBoxIs
+        '
+        Me.TextBoxIs.AllowDrop = True
+        Me.TextBoxIs.Location = New System.Drawing.Point(12, 36)
+        Me.TextBoxIs.Name = "TextBoxIs"
+        Me.TextBoxIs.ReadOnly = True
+        Me.TextBoxIs.Size = New System.Drawing.Size(242, 20)
+        Me.TextBoxIs.TabIndex = 0
+        Me.TextBoxIs.Text = "0."
+        Me.TextBoxIs.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'bt7
+        '
+        Me.bt7.ForeColor = System.Drawing.Color.Blue
+        Me.bt7.Location = New System.Drawing.Point(2, 129)
+        Me.bt7.Name = "bt7"
+        Me.bt7.Size = New System.Drawing.Size(41, 37)
+        Me.bt7.TabIndex = 1
+        Me.bt7.Text = "7"
+        Me.bt7.UseVisualStyleBackColor = True
+        '
+        'bt4
+        '
+        Me.bt4.ForeColor = System.Drawing.Color.Blue
+        Me.bt4.Location = New System.Drawing.Point(2, 172)
+        Me.bt4.Name = "bt4"
+        Me.bt4.Size = New System.Drawing.Size(41, 37)
+        Me.bt4.TabIndex = 2
+        Me.bt4.Text = "4"
+        Me.bt4.UseVisualStyleBackColor = True
+        '
+        'bt1
+        '
+        Me.bt1.ForeColor = System.Drawing.Color.Blue
+        Me.bt1.Location = New System.Drawing.Point(2, 215)
+        Me.bt1.Name = "bt1"
+        Me.bt1.Size = New System.Drawing.Size(41, 37)
+        Me.bt1.TabIndex = 3
+        Me.bt1.Text = "1"
+        Me.bt1.UseVisualStyleBackColor = True
+        '
+        'bt0
+        '
+        Me.bt0.ForeColor = System.Drawing.Color.Blue
+        Me.bt0.Location = New System.Drawing.Point(2, 258)
+        Me.bt0.Name = "bt0"
+        Me.bt0.Size = New System.Drawing.Size(41, 37)
+        Me.bt0.TabIndex = 4
+        Me.bt0.Text = "0"
+        Me.bt0.UseVisualStyleBackColor = True
+        '
+        'bt8
+        '
+        Me.bt8.ForeColor = System.Drawing.Color.Blue
+        Me.bt8.Location = New System.Drawing.Point(49, 129)
+        Me.bt8.Name = "bt8"
+        Me.bt8.Size = New System.Drawing.Size(41, 37)
+        Me.bt8.TabIndex = 5
+        Me.bt8.Text = "8"
+        Me.bt8.UseVisualStyleBackColor = True
+        '
+        'bt5
+        '
+        Me.bt5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.bt5.Location = New System.Drawing.Point(49, 172)
+        Me.bt5.Name = "bt5"
+        Me.bt5.Size = New System.Drawing.Size(41, 37)
+        Me.bt5.TabIndex = 6
+        Me.bt5.Text = "5"
+        Me.bt5.UseVisualStyleBackColor = True
+        '
+        'bt2
+        '
+        Me.bt2.ForeColor = System.Drawing.Color.Blue
+        Me.bt2.Location = New System.Drawing.Point(49, 215)
+        Me.bt2.Name = "bt2"
+        Me.bt2.Size = New System.Drawing.Size(41, 37)
+        Me.bt2.TabIndex = 7
+        Me.bt2.Text = "2"
+        Me.bt2.UseVisualStyleBackColor = True
+        '
+        'btpm
+        '
+        Me.btpm.ForeColor = System.Drawing.Color.Blue
+        Me.btpm.Location = New System.Drawing.Point(49, 258)
+        Me.btpm.Name = "btpm"
+        Me.btpm.Size = New System.Drawing.Size(41, 37)
+        Me.btpm.TabIndex = 8
+        Me.btpm.Text = "+/-"
+        Me.btpm.UseVisualStyleBackColor = True
+        '
+        'bt9
+        '
+        Me.bt9.ForeColor = System.Drawing.Color.Blue
+        Me.bt9.Location = New System.Drawing.Point(96, 129)
+        Me.bt9.Name = "bt9"
+        Me.bt9.Size = New System.Drawing.Size(41, 37)
+        Me.bt9.TabIndex = 9
+        Me.bt9.Text = "9"
+        Me.bt9.UseVisualStyleBackColor = True
+        '
+        'bt6
+        '
+        Me.bt6.ForeColor = System.Drawing.Color.Blue
+        Me.bt6.Location = New System.Drawing.Point(96, 172)
+        Me.bt6.Name = "bt6"
+        Me.bt6.Size = New System.Drawing.Size(41, 37)
+        Me.bt6.TabIndex = 10
+        Me.bt6.Text = "6"
+        Me.bt6.UseVisualStyleBackColor = True
+        '
+        'bt3
+        '
+        Me.bt3.ForeColor = System.Drawing.Color.Blue
+        Me.bt3.Location = New System.Drawing.Point(96, 215)
+        Me.bt3.Name = "bt3"
+        Me.bt3.Size = New System.Drawing.Size(41, 37)
+        Me.bt3.TabIndex = 11
+        Me.bt3.Text = "3"
+        Me.bt3.UseVisualStyleBackColor = True
+        '
+        'btpoint
+        '
+        Me.btpoint.ForeColor = System.Drawing.Color.Blue
+        Me.btpoint.Location = New System.Drawing.Point(96, 258)
+        Me.btpoint.Name = "btpoint"
+        Me.btpoint.Size = New System.Drawing.Size(41, 37)
+        Me.btpoint.TabIndex = 12
+        Me.btpoint.Text = ","
+        Me.btpoint.UseVisualStyleBackColor = True
+        '
+        'btdel
+        '
+        Me.btdel.ForeColor = System.Drawing.Color.Red
+        Me.btdel.Location = New System.Drawing.Point(143, 129)
+        Me.btdel.Name = "btdel"
+        Me.btdel.Size = New System.Drawing.Size(41, 37)
+        Me.btdel.TabIndex = 13
+        Me.btdel.Text = "/"
+        Me.btdel.UseVisualStyleBackColor = True
+        '
+        'btpr
+        '
+        Me.btpr.ForeColor = System.Drawing.Color.Red
+        Me.btpr.Location = New System.Drawing.Point(143, 172)
+        Me.btpr.Name = "btpr"
+        Me.btpr.Size = New System.Drawing.Size(41, 37)
+        Me.btpr.TabIndex = 14
+        Me.btpr.Text = "*"
+        Me.btpr.UseVisualStyleBackColor = True
+        '
+        'btmin
+        '
+        Me.btmin.ForeColor = System.Drawing.Color.Red
+        Me.btmin.Location = New System.Drawing.Point(143, 215)
+        Me.btmin.Name = "btmin"
+        Me.btmin.Size = New System.Drawing.Size(41, 37)
+        Me.btmin.TabIndex = 15
+        Me.btmin.Text = "-"
+        Me.btmin.UseVisualStyleBackColor = True
+        '
+        'btplus
+        '
+        Me.btplus.ForeColor = System.Drawing.Color.Red
+        Me.btplus.Location = New System.Drawing.Point(143, 258)
+        Me.btplus.Name = "btplus"
+        Me.btplus.Size = New System.Drawing.Size(41, 37)
+        Me.btplus.TabIndex = 16
+        Me.btplus.Text = "+"
+        Me.btplus.UseVisualStyleBackColor = True
+        '
+        'btButton4
+        '
+        Me.btButton4.ForeColor = System.Drawing.Color.Red
+        Me.btButton4.Location = New System.Drawing.Point(2, 81)
+        Me.btButton4.Name = "btButton4"
+        Me.btButton4.Size = New System.Drawing.Size(76, 27)
+        Me.btButton4.TabIndex = 17
+        Me.btButton4.Text = "BackSpace"
+        Me.btButton4.UseVisualStyleBackColor = True
+        '
+        'btce
+        '
+        Me.btce.ForeColor = System.Drawing.Color.Red
+        Me.btce.Location = New System.Drawing.Point(170, 81)
+        Me.btce.Name = "btce"
+        Me.btce.Size = New System.Drawing.Size(84, 27)
+        Me.btce.TabIndex = 18
+        Me.btce.Text = "CE"
+        Me.btce.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.ForeColor = System.Drawing.Color.Red
+        Me.Button1.Location = New System.Drawing.Point(84, 81)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(80, 27)
+        Me.Button1.TabIndex = 19
+        Me.Button1.Text = "C"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btis
+        '
+        Me.btis.ForeColor = System.Drawing.Color.Red
+        Me.btis.Location = New System.Drawing.Point(190, 258)
+        Me.btis.Name = "btis"
+        Me.btis.Size = New System.Drawing.Size(41, 37)
+        Me.btis.TabIndex = 20
+        Me.btis.Text = "="
+        Me.btis.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.ForeColor = System.Drawing.Color.Blue
+        Me.Button3.Location = New System.Drawing.Point(190, 215)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(41, 37)
+        Me.Button3.TabIndex = 21
+        Me.Button3.Text = "1/x"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.ForeColor = System.Drawing.Color.Blue
+        Me.Button2.Location = New System.Drawing.Point(190, 172)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(41, 37)
+        Me.Button2.TabIndex = 22
+        Me.Button2.Text = "%"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'sqrt
+        '
+        Me.sqrt.ForeColor = System.Drawing.Color.Blue
+        Me.sqrt.Location = New System.Drawing.Point(190, 129)
+        Me.sqrt.Name = "sqrt"
+        Me.sqrt.Size = New System.Drawing.Size(41, 37)
+        Me.sqrt.TabIndex = 23
+        Me.sqrt.Text = "sqrt"
+        Me.sqrt.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(266, 321)
+        Me.Controls.Add(Me.sqrt)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.btis)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btce)
+        Me.Controls.Add(Me.btButton4)
+        Me.Controls.Add(Me.btplus)
+        Me.Controls.Add(Me.btmin)
+        Me.Controls.Add(Me.btpr)
+        Me.Controls.Add(Me.btdel)
+        Me.Controls.Add(Me.btpoint)
+        Me.Controls.Add(Me.bt3)
+        Me.Controls.Add(Me.bt6)
+        Me.Controls.Add(Me.bt9)
+        Me.Controls.Add(Me.btpm)
+        Me.Controls.Add(Me.bt2)
+        Me.Controls.Add(Me.bt5)
+        Me.Controls.Add(Me.bt8)
+        Me.Controls.Add(Me.bt0)
+        Me.Controls.Add(Me.bt1)
+        Me.Controls.Add(Me.bt4)
+        Me.Controls.Add(Me.bt7)
+        Me.Controls.Add(Me.TextBoxIs)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBoxIs As TextBox
+    Friend WithEvents bt7 As Button
+    Friend WithEvents bt4 As Button
+    Friend WithEvents bt1 As Button
+    Friend WithEvents bt0 As Button
+    Friend WithEvents bt8 As Button
+    Friend WithEvents bt5 As Button
+    Friend WithEvents bt2 As Button
+    Friend WithEvents btpm As Button
+    Friend WithEvents bt9 As Button
+    Friend WithEvents bt6 As Button
+    Friend WithEvents bt3 As Button
+    Friend WithEvents btpoint As Button
+    Friend WithEvents btdel As Button
+    Friend WithEvents btpr As Button
+    Friend WithEvents btmin As Button
+    Friend WithEvents btplus As Button
+    Friend WithEvents btButton4 As Button
+    Friend WithEvents btce As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btis As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents sqrt As Button
+End Class
---
+++ 2 лаба лаба/Form1.vb
@@ -0,0 +1,148 @@
+﻿Public Class Form1
+    Private IsText As String
+    Private IsNumber As Double
+    Private Point As Boolean
+    Private op As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub bt1_Click(sender As Object, e As EventArgs) Handles bt1.Click
+        If IsText = "0" Or IsText = "+" Or IsText = "-" Or IsText = "/" Or IsText = "*" Then IsText = "1" Else IsText = IsText + "1"
+        TextBoxIs.Text = IsText
+    End Sub
+
+    Private Sub bt2_Click(sender As Object, e As EventArgs) Handles bt2.Click
+        If IsText = "0" Or IsText = "+" Or IsText = "-" Or IsText = "/" Or IsText = "*" Then IsText = "2" Else IsText = IsText + "2"
+        TextBoxIs.Text = IsText
+    End Sub
+
+    Private Sub btpm_Click(sender As Object, e As EventArgs) Handles btpm.Click
+        IsText = CStr(Val(IsText) * (-1))
+        TextBoxIs.Text = IsText
+    End Sub
+
+    Private Sub btpoint_Click(sender As Object, e As EventArgs) Handles btpoint.Click
+        If Point = False Then
+            IsText = IsText + "."
+            TextBoxIs.Text = IsText
+            Point = True
+        End If
+    End Sub
+
+    Private Sub btdel_Click(sender As Object, e As EventArgs) Handles btdel.Click
+        IsNumber = Val(TextBoxIs.Text)
+        op = 1
+        IsText = "/"
+        TextBoxIs.Text = IsText
+        Point = False
+    End Sub
+
+    Private Sub btpr_Click(sender As Object, e As EventArgs) Handles btpr.Click
+        IsNumber = Val(TextBoxIs.Text)
+        op = 2
+        IsText = "*"
+        TextBoxIs.Text = IsText
+        Point = False
+    End Sub
+
+    Private Sub btce_Click(sender As Object, e As EventArgs) Handles btce.Click
+        IsNumber = 0
+        IsText = "0"
+        TextBoxIs.Text = IsText
+        Point = False
+    End Sub
+
+    Private Sub bt3_Click(sender As Object, e As EventArgs) Handles bt3.Click
+        If IsText = "0" Or IsText = "+" Or IsText = "-" Or IsText = "/" Or IsText = "*" Then IsText = "3" Else IsText = IsText + "3"
+        TextBoxIs.Text = IsText
+    End Sub
+
+    Private Sub bt4_Click(sender As Object, e As EventArgs) Handles bt4.Click
+        If IsText = "0" Or IsText = "+" Or IsText = "-" Or IsText = "/" Or IsText = "*" Then IsText = "4" Else IsText = IsText + "4"
+        TextBoxIs.Text = IsText
+    End Sub
+
+    Private Sub bt5_Click(sender As Object, e As EventArgs) Handles bt5.Click
+        If IsText = "0" Or IsText = "+" Or IsText = "-" Or IsText = "/" Or IsText = "*" Then IsText = "5" Else IsText = IsText + "5"
+        TextBoxIs.Text = IsText
+    End Sub
+
+    Private Sub bt6_Click(sender As Object, e As EventArgs) Handles bt6.Click
+        If IsText = "0" Or IsText = "+" Or IsText = "-" Or IsText = "/" Or IsText = "*" Then IsText = "6" Else IsText = IsText + "6"
+        TextBoxIs.Text = IsText
+    End Sub
+
+    Private Sub bt7_Click(sender As Object, e As EventArgs) Handles bt7.Click
+        If IsText = "0" Or IsText = "+" Or IsText = "-" Or IsText = "/" Or IsText = "*" Then IsText = "7" Else IsText = IsText + "7"
+        TextBoxIs.Text = IsText
+    End Sub
+
+    Private Sub bt8_Click(sender As Object, e As EventArgs) Handles bt8.Click
+        If IsText = "0" Or IsText = "+" Or IsText = "-" Or IsText = "/" Or IsText = "*" Then IsText = "8" Else IsText = IsText + "8"
+        TextBoxIs.Text = IsText
+    End Sub
+
+    Private Sub bt9_Click(sender As Object, e As EventArgs) Handles bt9.Click
+        If IsText = "0" Or IsText = "+" Or IsText = "-" Or IsText = "/" Or IsText = "*" Then IsText = "9" Else IsText = IsText + "9"
+        TextBoxIs.Text = IsText
+    End Sub
+
+    Private Sub btmin_Click(sender As Object, e As EventArgs) Handles btmin.Click
+        IsNumber = Val(TextBoxIs.Text)
+        op = 3
+        IsText = "-"
+        TextBoxIs.Text = IsText
+        Point = False
+    End Sub
+
+    Private Sub btplus_Click(sender As Object, e As EventArgs) Handles btplus.Click
+        IsNumber = Val(TextBoxIs.Text)
+        op = 4
+        IsText = "+"
+        TextBoxIs.Text = IsText
+        Point = False
+    End Sub
+
+    Private Sub bt0_Click(sender As Object, e As EventArgs) Handles bt0.Click
+        If IsText = "0" Or IsText = "+" Or IsText = "-" Or IsText = "/" Or IsText = "*" Then IsText = "0" Else IsText = IsText + "0"
+        TextBoxIs.Text = IsText
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        IsText = IsText.Remove(IsText.Length - 1, 1)
+        TextBoxIs.Text = IsText
+    End Sub
+
+    Private Sub btis_Click(sender As Object, e As EventArgs) Handles btis.Click
+        Select Case op
+            Case 1
+                IsNumber = IsNumber / Val(TextBoxIs.Text)
+            Case 2
+                IsNumber = IsNumber * Val(TextBoxIs.Text)
+            Case 3
+                IsNumber = IsNumber - Val(TextBoxIs.Text)
+            Case 4
+                IsNumber = IsNumber + Val(TextBoxIs.Text)
+        End Select
+        IsText = CStr(IsNumber)
+        TextBoxIs.Text = IsText
+        IsText = ""
+        Point = False
+    End Sub
+
+    Private Sub sqrt_Click(sender As Object, e As EventArgs) Handles sqrt.Click
+        IsNumber = Math.Sqrt(TextBoxIs.Text)
+        TextBoxIs.Text = IsNumber
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        IsNumber = 1 / Val(TextBoxIs.Text)
+        TextBoxIs.Text = IsNumber
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        IsNumber = Val(TextBoxIs.Text) / 100
+        TextBoxIs.Text = IsNumber
+    End Sub
+End Class
